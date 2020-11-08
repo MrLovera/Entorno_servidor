f5ee28eea405208f5d9723f5e3db6eac304bef5e
MrLovera <mrlovera98@gmail.com>
2020-11-08 05:34:07 +0000

    Caso Practico

---
+++ adios.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Adios</title>
+  </head>
+  <body>
+    <?php echo "Hasta luego desarrollador!!!";
+     ?>
+  </body>
+</html>
---
+++ hola.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Primer php</title>
+  </head>
+  <body>
+    <?php echo "Hola Paco campeon!!!!";
+     ?>
+
+     
+<br>
+<a href="adios.php">Adios</a>
+  </body>
+</html>
